dcbc1b39421b3a03c0302bdd47204d42c034d0d1
FriedrichFröbel <FriedrichFroebel.OC@t-online.de>
2020-12-05 21:40:27 +0100

    fix syntax error and formatting

--- src/main/java/cmanager/oc/OcUtil.java
+++ src/main/java/cmanager/oc/OcUtil.java
@@ -151,9 +151,10 @@ public class OcUtil {
                     Okapi.getCachesAround(user, uuid, geocache, searchRadius, OKAPI_RUNTIME_CACHE);
 
             if (similar == null) {
-                LOGGER.info(MessageFormat.format("Found no candidates for {0}." geocache.getCode()));
+                LOGGER.info(
+                        MessageFormat.format("Found no candidates for {0}.", geocache.getCode()));
                 SearchCache.setEmptySearch(geocache, uuid);
-                return;
+                return null;
             }
             LOGGER.info(
                     MessageFormat.format(
